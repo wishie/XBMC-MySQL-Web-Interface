daafef53f7b3cc506dd52ca9c651dfb567107cd7
Philipp Zeitschel <philipp@zeitschel.net>
2011-09-01 14:01:39 +0200

    added Javascript, HTML-Header, UTF-8 encoding, rewritten MYSQL Querys and Methods
    added color box to show movie details without reload added javascript to
    load more movies on scrolling down added a complete HTML-Header added
    complete UTF-8 encoding rewritten mysql statements to use mysqli
    rewritten querys for better performance

--- medialists/index.php
+++ medialists/index.php
@@ -1,12 +1,10 @@
-<link rel="stylesheet" type="text/css" href="styles.css" />
 <?php
 
 require('access.php');
 require('config.php');
 
-$counter = "0";
 $cellbg  = "0";
-$limit	 = "15";
+$limit	 = "50";
 
 function thumbnailHash($input) {
         $chars = strtolower($input);
@@ -35,216 +33,265 @@ function thumbnailHash($input) {
         }
 }
 
-mysql_pconnect($hostname, $db_user, $db_pass);
-mysql_select_db($database);
+$db = @new mysqli($hostname, $db_user, $db_pass, $database);
+//$db->query("SET NAMES utf8");
 
-$totalmovies = mysql_fetch_array(mysql_query("SELECT COUNT(*) FROM movie"));
-if(!isset($_POST["startnum"])){ $_POST["startnum"] = "0" ;};
-if($_GET["startnum"] == ""){ $_GET["startnum"] = "0";};
-if($_GET["startnum"] > $totalmovies[0]){
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+//count movies
+$result = $db->query("SELECT COUNT(*) FROM movie");
+$totalmovies = $result->fetch_row();
+
+if (!$_REQUEST['get'])
+{
+    echo '
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Language" content="en" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+    <link rel="stylesheet" href="css/colorbox.css" type="text/css" />
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.colorbox-min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+            last = 0;
+            $("a").colorbox();
+            $("#non-javascript").hide();
+            $(window).scroll(function(){
+                if  ($(window).scrollTop() == $(document).height() - $(window).height()){
+                    if (last <= "'.$totalmovies[0].'")
+                    {
+                        $(\'#mainlist\').append(\'<div id="loading"><img src="img/loading.gif" /></div>\');
+                       last = last+50;
+                       $.get("index.php?get=" + last, function(data) {
+                        $(\'#loading\').remove();
+                        $(\'#mainlist\').append(data);
+                       });
+                    }
+                }
+            }); 
+        });
+    </script>
+    <title>MediaList</title>
+</head>
+<body>
+    <table width="100%" cellpadding="0" cellspacing="0" border="0">
+        <tr class="mainlist">
+            <td colspan="3">
+                <a class="nav" href="?action=logout">LOGOUT</a>
+                <h1>Movie list '.$titel_plus.'</h1>
+            </td>
+        </tr>
+        <tr class="mainlist">
+            <td colspan="3">
+                Search: <form method="post" action="?search">
+                    <input type="text" name="searchterm">
+                    <input type="radio" name="st" value="titlesearch" checked>Title Search</option>
+                    <input type="radio" name="st" value="genresearch">Genre Search</option>
+                    <input type="submit" name="submit" value="Go">
+                </form>
+            </td>
+        </tr>
+    </table>
+    <div id="mainlist">
+        ';
+}
+
+//set next movies to load
+if(!isset($_REQUEST["startnum"])){ $_REQUEST["startnum"] = "0"; }
+if($_REQUEST["startnum"] > $totalmovies[0])
+{
 	$startnum = "0";
-}ELSE{
-	$startnum = $_POST["startnum"];
-};
+}
+else
+{
+	$startnum = $_REQUEST["startnum"];
+}
 
-if(!isset($_GET['all'])){
-	$query  = "SELECT * FROM movie ORDER BY c00 LIMIT $startnum, $limit";
-}else{
-	$query	= "SELECT * FROM movie";
-	if(isset($_GET['search'])){
-		$searchterm = $_POST['searchterm'];
-		if($_POST['st'] == "titlesearch"){
-			$query .= " WHERE c00 LIKE '%$searchterm%'";
-		}
-		if($_POST['st'] == "genresearch"){
-			$query .= " WHERE c14 LIKE '%$searchterm%'";
-		}
-	}
+if ($_REQUEST['get'])
+{
+    $startnum = $_REQUEST['get'];
+}
+
+//get movies
+if(!isset($_REQUEST['searchterm']))
+{
+	$query  = "SELECT 
+    strPath AS path, 
+    strFileName AS filename, 
+    idMovie AS movieid, 
+    c00 AS title, 
+    c01 AS plot,
+    c03 AS tagline,
+    c07 AS year,
+    c11 AS runtime,
+    c14 AS genre
+    FROM movieview
+    ORDER BY c00 
+    LIMIT $startnum, $limit";
+}
+else
+{
+    //do search
+    if($_REQUEST['st'] == "titlesearch"){
+        $titel_plus = "(Title Search: ".$_REQUEST['searchterm']." )"; 
+    }
+    if($_REQUEST['st'] == "genresearch"){
+        $titel_plus = "(Genre Search: ".$_REQUEST['searchterm']." )"; 
+    }
+	$query	= "SELECT 
+    strPath AS path, 
+    strFileName AS filename, 
+    idMovie AS movieid, 
+    c00 AS title, 
+    c01 AS plot,
+    c03 AS tagline,
+    c07 AS year,
+    c11 AS runtime,
+    c14 AS genre
+    FROM movieview";
+    $searchterm = $_REQUEST['searchterm'];
+    if($_REQUEST['st'] == "titlesearch")
+    {
+        $query .= " WHERE c00 LIKE '%$searchterm%'";
+    }
+    if($_REQUEST['st'] == "genresearch")
+    {
+        $query .= " WHERE c14 LIKE '%$searchterm%'";
+    }
 	$query .= " ORDER BY c00";
 }
-$result = mysql_query($query);
-?>
 
-<table width="100%" cellpadding="0" cellspacing="0" border="0">
-	<tr class="mainlist">
-		<td colspan="3">
-			<h1>Movie list
-			<?php
-			if($_POST['searchterm'] == ""){
-				unset($_POST['searchterm']);
-			}
-			if(isset($_POST['searchterm'])){ 
-				if($_POST['st'] == "titlesearch"){
-				echo "(Title Search: ".$_POST['searchterm']." )"; 
-				}
-				if($_POST['st'] == "genresearch"){
-				echo "(Genre Search: ".$_POST['searchterm']." )"; 
-				}
-			}
-			?>
-			</h1>
-		</td>
-	</tr>
-	<tr class="mainlist">
-		<td colspan="3">
-		Search: <form method="post" action="?all&search">
-		<input type="text" name="searchterm">
-		<input type="radio" name="st" value="titlesearch" checked>Title Search</option>
-		<input type="radio" name="st" value="genresearch">Genre Search</option>
-		<input type="submit" name="submit" value="Go">
-		</form></td>
-	</tr>
-	<tr class="mainlist">
-<?php
-while($row = mysql_fetch_row($result))
-{
-    $movieid	= $row[0];
-    $fileid	= $row[1];
-    $title   	= $row[2];
-    $plot 	= $row[3];
-    $outline 	= $row[4];
-    $tagline 	= $row[5];
-    $numvotes	= $row[6];
-    $rating	= $row[7];
-    $writer	= $row[8];
-    $year 	= $row[9];
-    $thumb 	= $row[10];
-    $imdbid	= $row[11];
+$result = $db->query($query);
 
-    $runtime	= $row[13];
-    $runtimeh   = floor(substr($runtime, 0, 3) / 60);
-    $runtimem   = $runtime - ($runtimeh * 60);
+while($row = $result->fetch_assoc())
+{   
+    $title   	= htmlentities($row[title]);
+    $plot 	= htmlentities($row[plot]);  
+    $tagline 	= htmlentities($row[tagline]);
+    $genre	= htmlentities($row[genre]);
+    
+    $runtimeh   = floor(substr($row[runtime], 0, 3) / 60);
+    $runtimem   = $row[runtime] - ($runtimeh * 60);
 
-    $rated	= $row[14];
-
-    $genre	= $row[16];
-    $director	= $row[17];
-
-    $studio	= $row[20];
-
-    $fanart	= $row[22];
-
-    $resquery	= mysql_query("SELECT iVideoHeight AS vertres, iVideoWidth AS horizres, strAudioCodec AS acodec, iAudioChannels AS achannels FROM streamdetails WHERE idFile = '$fileid' AND iStreamType = '0'");
-    $resquery2	= mysql_query("SELECT strAudioCodec AS acodec, iAudioChannels AS achannels FROM streamdetails WHERE idFile = '$fileid' AND iStreamType = '1'");
-    $resresult	= mysql_fetch_row($resquery);
-    $resresult2	= mysql_fetch_row($resquery2);
-    $vertres	= $resresult[0];
-    $horizres	= $resresult[1];
-    $acodec	= $resresult2[0];
-    $achannels	= $resresult2[1];
-
-    $filequery	= mysql_query("SELECT strPath AS path, strFileName AS filename FROM movieview WHERE idFile = '$fileid'");
-    $fileresult	= mysql_fetch_row($filequery);
-    $filepath	= $fileresult[0];
-    $filename	= $fileresult[1];
+    $filepath	= $row[path];
+    $filename	= $row[filename];
     if(substr($filename, 0, 8) == "stack://"){
-    $filename   = explode(",", $filename);
-    $filename   = str_replace("$filepath", "", $filename[0]);
-    $filename   = str_replace("stack://", "", $filename);
-    ;};
+	    $filename   = explode(",", $filename);
+	    $filename   = str_replace("$filepath", "", $filename[0]);
+	    $filename   = str_replace("stack://", "", $filename);
+    }
     $fullpath   = trim($filepath.$filename);
-    $finalfile  = $fullpath;
+    $finalfile  = utf8_encode($fullpath);
     $playfile   = urlencode($finalfile);
-    $thumbhash	= thumbnailHash($finalfile);
-    $cellbg++;
-    if($cellbg % 2 == 0){
-?>
+    $thumbhash	= substr(thumbnailHash($finalfile),-8);
 
-	<td width="33%">
-<?php }ELSE{ ?>
-	<td width="33%" bgcolor="#EEEEEE">
-<?php }; ?>
-		<table cellpadding="0" cellspacing="0" border="0" width="100%">
-			<tr>
-				<td width="75">
-					<a href="movieinfo.php?movieid=<?php echo $movieid;?>"><img border="0" width="75" src="Thumbnails/Video/<?php echo $thumbhash[0];?>/<?php echo $thumbhash;?>.tbn"></a>
-				</td>
-				<td width="10">&nbsp;</td>
-				<td class="mainlist" valign="top">
-					<table cellpadding="0" cellspacing="0" border="0">
-						<tr>
-							<td width="300"><a class="nav" href="movieinfo.php?movieid=<?php echo $movieid;?>"><?php echo $title; if($year != "0") echo " (".$year.") ";?></a></td>
-							<td width="75" class="mainlist"><?php echo $runtimeh."hr ".$runtimem."mins";?></td>
-						</tr>
-						<tr height="70" valign="top">
-							<td colspan="2" class="mainlist"><?php echo $genre;?><br><br>
-							<?php if(strlen($tagline) > "150"){echo substr($tagline, 0, 150)."...";}else{echo $tagline;};?>
-							</td>
-						</tr>
-						<tr height="10">
-							<td colspan="2" class="mainlist" align="right"><a class="nav" href="movieinfo.php?movieid=<?php echo $movieid;?>">More info...</a></td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-		</table>
-	</td>
-<?php 
-	$counter++;
-	if($counter % 3 == 0){
-		echo "</tr><tr>";
-		$counter = 0;
-	}
+    if($cellbg == 1)
+    {
+        $cellbg = 0;
+        echo '
+        <div style="min-width: 275px; height: 115px; float:left;">';
+    }
+    else
+    {
+        $cellbg = 1;
+	    echo '
+	<div style="min-width: 275px; height: 115px; background-color: #EEEEEE; float:left;">';
+    }
+
+	echo '
+            <div style="float:left;">
+                <a href="movieinfo.php?movieid='.$row[movieid].'"><img border="0" style="max-width: 75px; max-height: 112px;" src="Thumbnails/Video/'.$thumbhash[0].'/'.$thumbhash.'.tbn"></a>
+            </div>
+            <div style="padding-left: 85px;">
+                <table cellpadding="0" cellspacing="0" border="0">
+                    <tr>
+                        <td width="200">
+                            <a class="nav" href="movieinfo.php?movieid='.$row[movieid].'">'.$title; if($row[year] != "0") { echo " (".$row[year].") "; }
+                        echo '<td width="75" class="mainlist">'.$runtimeh.' hr '.$runtimem.' mins</td>
+                    </tr>
+                    <tr height="70" valign="top">
+                        <td colspan="2" class="mainlist">'.$genre.'<br><br>
+                        ';
+                        if(strlen($tagline) > "150")
+                        {
+                            echo substr($tagline, 0, 150)."...";
+                        }
+                        else
+                        {
+                            echo $tagline;
+                        }
+                        echo '
+                        </td>
+                    </tr>
+                    <tr height="10">
+                        <td colspan="2" class="mainlist" align="right"><a class="nav" href="movieinfo.php?movieid='.$row[movieid].'">More info...</a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        ';
+}
+
+if (!$_REQUEST['get'])
+{
+    echo '
+    </div>
+    <div id="non-javascript" style="clear:both">
+        <table width="100%" cellpadding="0" cellspacing="0" border="0">
+            <tr>
+                <td align="center" class="mainlist">
+                    <form method="post" action="index.php">
+                        PAGE:
+                        <select name="startnum">';
+                            
+                            if(!isset($page))
+                            {
+                                $page = "1";
+                            }
+                            $pages = ceil($totalmovies[0] / $limit + 1);
+                            while($page < $pages)
+                            {
+                                $nextpage = $page * $limit - $limit;
+                                if($startnum == $nextpage){
+                                    echo "<option selected value=\"$nextpage\">$page</option>";
+                                }
+                                else
+                                {
+                                    echo "<option value=\"$nextpage\">$page</option>";
+                                };
+                                $page++;
+                            };
+                            $next = $startnum + $limit;
+                            $startnum++;
+                        echo '
+                        </select>
+                        <input type="submit" value="Go" name="submit">
+                    </form>
+                    Displaying movies '.$startnum.' to '.$next.' of '.$totalmovies[0].'
+                </td>
+            </tr>';
+                    
+            if(isset($_REQUEST['search'])){
+                echo '
+                <tr>
+                    <td><center><br><a class="nav" href="index.php">BACK TO MAIN PAGE</a></center></td>
+                </tr>';
+            }
+            echo '
+            <tr>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>';
+}
+mysqli_close($db);
 ?>
-	
-<?php }; ?>
-</table>
-<table width="100%" cellpadding="0" cellspacing="0" border="0">
-	<tr>
-		<td align="center" class="mainlist">
-			<?php if(!isset($_GET['all'])){ ?>
-			<form method="post" action="index.php">
-			PAGE:
-			<select name="startnum">
-			<?php
-			if(!isset($page)){
-				$page = "1";
-			}
-			$pages = ceil($totalmovies[0] / $limit + 1);
-			while($page < $pages){
-			$nextpage = $page * $limit - $limit;
-			if($startnum == $nextpage){
-			echo "<option selected value=\"$nextpage\">$page</option>";
-			}else{
-			echo "<option value=\"$nextpage\">$page</option>";
-			};
-			$page++;}; ?>
-			</select>
-			<input type="submit" value="Go" name="submit">
-			</form>
-			Displaying movies <?php echo $startnum + 1;?> to <?php echo $startnum + $limit;?> of <?php echo $totalmovies[0];?>
-			<?php }; ?>
-		</td>
-	</tr>
-	<?php if(isset($_GET['search'])){ ?>
-	<tr>
-		<td><center><br><a class="nav" href="index.php">BACK TO MAIN PAGE</a></center></td>
-	</tr>
-	<?php ;}; ?>
-	<tr>
-		<td class="mainlist">
-			<?php
-			    if(isset($_GET['stop'])){
-			        $fh = fopen('/dev/null', 'w');
-			        $ch = curl_init();
-			        if(!isset($_GET['location'])){
-			                $_GET['location'] = "lounge";
-			        };
-			        if($_GET['location'] == "lounge"){
-			                curl_setopt($ch, CURLOPT_URL, "http://$xbmc_lounge/xbmcCmds/xbmcHttp?command=ExecBuiltIn(PlayerControl(stop))");
-			        };
-			        curl_setopt($ch, CURLOPT_FILE, $fh);
-			        curl_exec($ch);
-			        curl_close($ch);
-			        fclose($fh);
-			    };
-			?>
-			<center>
-			Stop playback: 
-			<a class="nav" href="?stop&location=lounge">Lounge</a>
-			<br>
-			<a class="nav" href="?action=logout">LOGOUT</a>
-			</center>
-		</td>
-	</tr>
-</table>
